2865e5516862b82c46d4af742639996545129bd6
Niyi Gbodimowo <niyi.gbodimowo@pmglobaltechnology.com>
2017-09-25 18:23:08 +0100

    Fixed the logo click problem

--- scripts/website.js
+++ scripts/website.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     var headerEl = document.querySelector('.book-header');
     var summaryEl = document.querySelector('.book.with-summary');
     summaryEl.addEventListener('click', function (e) {
-        if (e.offsetY < headerEl.offsetHeight) {
-            document.location = 'https://formelo.com'
+        if (e.srcElement === summaryEl && e.offsetY < headerEl.offsetHeight) {
+            document.location = 'https://formelo.com';
         }
     });
 });
